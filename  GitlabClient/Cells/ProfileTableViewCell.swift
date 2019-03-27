c49ee3d2e8bb7efbbedab3ed48b63df34df2d964
Andrew Mykytiuk <dromabro777@gmail.com>
2019-03-27 15:16:48 +0200

    Add UI for User profile info

---
+++  GitlabClient/Cells/ProfileTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileTableViewCell.swift
+//  GitlabClient
+//
+//  Created by User on 26/04/2019.
+//  Copyright © 2019 MPTechnologies. All rights reserved.
+//
+
+import UIKit
+
+class ProfileTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var dataLabel: UILabel!
+
+}
---  GitlabClient/ViewControllers/ProfileViewController.swift
+++  GitlabClient/ViewControllers/ProfileViewController.swift
@@ -18,16 +18,13 @@ class ProfileViewController: BaseViewController {
     @IBOutlet weak var avatarImage: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var statusLabel: UILabel!
-    @IBOutlet weak var emailTF: UITextField!
-    @IBOutlet weak var locationTF: UITextField!
-    @IBOutlet weak var skypeTF: UITextField!
-    @IBOutlet weak var linkedinTF: UITextField!
-    @IBOutlet weak var twitterTF: UITextField!
-    @IBOutlet weak var websiteUrlTF: UITextField!
+    @IBOutlet weak var profileTableView: UITableView!
     
     weak var delegate: ProfileViewControllerDelegate?
     private var loginService: LoginService!
     private var profileService: ProfileService!
+    private var data: [String?] = []
+    private var labels: [String] = []
     
     func configure(with loginService: LoginService, profileService: ProfileService) {
         self.loginService = loginService
@@ -42,6 +39,9 @@ class ProfileViewController: BaseViewController {
             switch result {
             case .success(let user):
                 self.setup(with: user)
+                DispatchQueue.main.async {
+                    self.profileTableView.reloadData()
+                }
             case .error(let error):
                 let alert = AlertHelper.createErrorAlert(message: error.localizedDescription, handler: nil)
                 self.present(alert, animated: true)
@@ -50,28 +50,27 @@ class ProfileViewController: BaseViewController {
         
     }
     
-//    override func viewDidLayoutSubviews() {
-//        super.viewDidLayoutSubviews()
-//        avatarImage.layer.cornerRadius = avatarImage.frame.height / 2
-//    }
-    
     func setup (with user:User) {
         if let data = try? Data(contentsOf: user.avatarUrl) {
         DispatchQueue.main.async {
             self.avatarImage.image = UIImage(data: data)
-        }
-        }
-        DispatchQueue.main.async {
             self.nameLabel.text = user.name
             self.statusLabel.text = user.bio
-            self.emailTF.text = user.email
-            self.locationTF.text = user.location
-            self.skypeTF.text = user.skype
-            self.linkedinTF.text = user.linkedin
-            self.twitterTF.text = user.twitter
-            self.websiteUrlTF.text = user.websiteUrl
-        }
-       
+        }}
+        self.data.append(user.email)
+        self.data.append(user.location)
+        self.data.append(user.skype)
+        self.data.append(user.linkedin)
+        self.data.append(user.twitter)
+        self.data.append(user.websiteUrl)
+        
+        self.labels.append(tableViewLabels.email.rawValue)
+        self.labels.append(tableViewLabels.location.rawValue)
+        self.labels.append(tableViewLabels.skype.rawValue)
+        self.labels.append(tableViewLabels.linkdn.rawValue)
+        self.labels.append(tableViewLabels.twitter.rawValue)
+        self.labels.append(tableViewLabels.website.rawValue)
+        
     }
 
     @IBAction func logOutButtonAction(_ sender: UIButton) {
@@ -88,5 +87,51 @@ class ProfileViewController: BaseViewController {
         }
         
     }
+    
+}
+
+extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    private enum tableViewLabels: String {
+        case email = "Email"
+        case location = "Location"
+        case skype = "Skype"
+        case linkdn = "Linkedin"
+        case twitter = "Twitter"
+        case website = "Website"
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileTableViewCell", for: indexPath) as? ProfileTableViewCell else {
+            fatalError("The dequeued cell is not an instance of ProfileTableViewCell.")
+        }
+            cell.infoLabel.text = labels[indexPath.row]
+            cell.dataLabel.text = data[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let attributes = [NSAttributedString.Key.font:UIFont(name: "Symbol", size: 17)]
+        let string = data[indexPath.row]
+        
+        guard let text = string, text.count > 0 else  {
+            return 0
+        }
+        
+        let rect = NSString(string: text).boundingRect(
+            with: CGSize(width: tableView.frame.width, height: CGFloat.greatestFiniteMagnitude),
+            options: [.usesLineFragmentOrigin, .usesFontLeading],
+            attributes: attributes as [NSAttributedString.Key : Any], context: nil)
+        
+        let height = 8 + rect.height + 8
+        
+        return height
+    }
+    
     
 }
