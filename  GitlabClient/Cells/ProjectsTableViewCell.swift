edec803439acb385f8103882cf4b63ab8ffbb3a0
Andrew Mykytiuk <dromabro777@gmail.com>
2019-04-25 16:38:32 +0300

    Add 1 line author and assignee labels support for all orientations

---  GitlabClient/Cells/ProjectsTableViewCell.swift
+++  GitlabClient/Cells/ProjectsTableViewCell.swift
@@ -24,6 +24,8 @@ class ProjectsTableViewCell: UITableViewCell {
             numberOfLinesHeight = mergesLabelDescription.font.lineHeight * CGFloat(numberOfLines)
         }
     }
+    @IBOutlet private weak var authorOneLineLabel: UILabel!
+    @IBOutlet private weak var assignToOneLineLabel: UILabel!
     @IBOutlet private weak var authorLabel: UILabel!
     @IBOutlet private weak var assignToLabel: UILabel!
     @IBOutlet private weak var authorNameLabel: UILabel!
@@ -53,12 +55,16 @@ class ProjectsTableViewCell: UITableViewCell {
         self.assignToLabel.text = localStrings.assign
         self.authorNameLabel.text = request.author.name
         self.assignToNameLabel.text = request.assignee.name
+        self.authorOneLineLabel.text = request.author.name
+        self.assignToOneLineLabel.text = request.assignee.name
         
         let mergeRequestDescriptionHeight = TextHelper.getHeightForStringInLabel(with: request.description, width: mergesLabelDescription.frame.width)
         let isButtonNeeded = mergeRequestDescriptionHeight > numberOfLinesHeight
         
         setupForButton(isNeeded: isButtonNeeded, isExpanded: isExpanded)
         self.mergesLabelDescription.text = request.description
+        
+        let _ = calculateSameLineForAuthorAndAssignee(authorName: request.author.name, assigneeName: request.assignee.name)
     }
     
     private func setupForButton(isNeeded: Bool, isExpanded: Bool) {
@@ -81,17 +87,41 @@ class ProjectsTableViewCell: UITableViewCell {
         let names = self.getAuthorAndAssignTitles()
         
         let mergeRequestTitleHeight = TextHelper.getHeightForStringInLabel(with: request.title, width: mergesLabel.frame.width)
-        let authorNameHeight = TextHelper.getHeightForStringInLabel(with: names.assign, width: authorLabel.frame.width)
-        let assignToHeight = TextHelper.getHeightForStringInLabel(with: names.author, width: assignToLabel.frame.width)
+        let authorNameHeight = TextHelper.getHeightForStringInLabel(with: names.author, width: authorLabel.frame.width)
+        let assignToHeight = TextHelper.getHeightForStringInLabel(with: names.assign, width: assignToLabel.frame.width)
         let authorDataHeight = TextHelper.getHeightForStringInLabel(with: request.author.name, width: authorNameLabel.frame.width)
         let assignDataHeight = TextHelper.getHeightForStringInLabel(with: request.assignee.name, width: assignToNameLabel.frame.width)
         let (mergeRequestDescriptionHeight, isButtonNeeded) = calculatingHeightForDescriptionLabel(with: request.description, isExpanded: isExpanded)
         
-        height = ceil(mergeRequestTitleHeight) + max(authorNameHeight, assignToHeight) + max(authorDataHeight, assignDataHeight) + ceil(mergeRequestDescriptionHeight)
+        if calculateSameLineForAuthorAndAssignee(authorName: request.author.name, assigneeName: request.assignee.name) {
+            height = ceil(mergeRequestTitleHeight) + max(authorNameHeight, assignToHeight) + ceil(mergeRequestDescriptionHeight)
+        } else {
+            height = ceil(mergeRequestTitleHeight) + max(authorNameHeight, assignToHeight) + max(authorDataHeight, assignDataHeight) + ceil(mergeRequestDescriptionHeight)
+        }
         
         height += self.cellOffsets(isNeeded: isButtonNeeded)
         
         return height
+    }
+    
+    private func calculateSameLineForAuthorAndAssignee(authorName: String, assigneeName: String) -> Bool {
+        
+        let authorDataHeight = TextHelper.getHeightForStringInLabel(with: authorName, width: authorOneLineLabel.frame.width)
+        let assignDataHeight = TextHelper.getHeightForStringInLabel(with: assigneeName, width: assignToOneLineLabel.frame.width)
+        
+        authorOneLineLabel.isHidden = authorDataHeight > authorOneLineLabel.font.lineHeight
+        assignToOneLineLabel.isHidden = assignDataHeight > assignToOneLineLabel.font.lineHeight
+        
+        if !authorOneLineLabel.isHidden && !assignToOneLineLabel.isHidden {
+            authorNameLabel.isHidden = !assignToOneLineLabel.isHidden
+            assignToNameLabel.isHidden = !assignToOneLineLabel.isHidden
+            return !assignToOneLineLabel.isHidden
+        } else {
+            authorNameLabel.isHidden = false
+            assignToNameLabel.isHidden = false
+            return false
+        }
+        
     }
     
     private func calculatingHeightForDescriptionLabel(with string: String, isExpanded: Bool) -> (CGFloat, Bool) {
@@ -100,7 +130,7 @@ class ProjectsTableViewCell: UITableViewCell {
         let isButtonNeeded = mergeRequestDescriptionHeight > numberOfLinesHeight
         
         if !isExpanded {
-            return (numberOfLinesHeight, isButtonNeeded)
+            return ( mergeRequestDescriptionHeight < numberOfLinesHeight ? mergeRequestDescriptionHeight : numberOfLinesHeight, isButtonNeeded)
         }
         
         return (mergeRequestDescriptionHeight, isButtonNeeded)
