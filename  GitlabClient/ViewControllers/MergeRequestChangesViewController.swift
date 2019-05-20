4a7f224449e0d7100ca1a0c8fa3100f657e1ab41
amikityuk <Andrey.Mikityuk@dataart.com>
2019-05-20 14:13:58 +0300

    GCG-16. [As a user I want to be able to review the diff of the selected file]
    Update Regexes - right one for +/- strings

---  GitlabClient/ViewControllers/MergeRequestChangesViewController.swift
+++  GitlabClient/ViewControllers/MergeRequestChangesViewController.swift
@@ -16,6 +16,22 @@ class MergeRequestChangesViewController: BaseViewController {
     private let activityIndicator = UIActivityIndicatorView(style: .whiteLarge)
     private var mergeRequestChange: MergeRequestChanges?
     
+    private enum PatternsWithColors {
+        
+        case added
+        case deleted
+        
+        var info: (pattern: String, color: UIColor) {
+            switch self {
+            case .added:
+                return ("^[+]\\D(.+)$", Constants.Colors.mainGreen.value)
+            case .deleted:
+                return ("^[-]\\D(.+)$", Constants.Colors.mainRed.value)
+            }
+        }
+        
+    }
+    
     func configureMergeRequestChangesInfo(change: MergeRequestChanges) {
         self.mergeRequestChange = change
         self.navigationController?.title = change.newPath
@@ -53,85 +69,36 @@ class MergeRequestChangesViewController: BaseViewController {
     private func setUpData(change: MergeRequestChanges) {
         let attributes = [NSAttributedString.Key.font : Constants.font]
         let attribute = NSMutableAttributedString(string: change.diff, attributes: attributes as [NSAttributedString.Key : Any])
-        let lines = change.diff.components(separatedBy: "\t")
         
         switch change.state {
         case .new:
-            setUpColorForView(self.view, with: Constants.Colors.mainGreen)
+            setUpColorForView(self.view, with: .mainGreen)
         case .deleted:
-            setUpColorForView(self.view, with: Constants.Colors.mainRed)
+            setUpColorForView(self.view, with: .mainRed)
         case .modified:
-            let deletedRegex = try? NSRegularExpression(pattern: "[-]\\D(.+?)(\n)")
-            let addedRegex = try? NSRegularExpression(pattern: "[+]\\D(.+?)(\n)")
-            
-            findAndHighliteText(with: deletedRegex, string: change.diff, attribute: attribute, with: .mainRed)
-            findAndHighliteText(with: addedRegex, string: change.diff, attribute: attribute, with: .mainGreen)
+            findAndHighliteText(with: .added, string: change.diff, attribute: attribute)
+            findAndHighliteText(with: .deleted, string: change.diff, attribute: attribute)
         }
         self.MRChangesTextView.attributedText = attribute
     }
     
-    private func findAndHighliteText(with regex: NSRegularExpression?, string: String, attribute: NSMutableAttributedString, with color: Constants.Colors) {
-       let regexOptions: NSRegularExpression.MatchingOptions = [.withoutAnchoringBounds]
-        guard let matches = regex?.matches(in: string, options: regexOptions, range: NSRange(string.startIndex..., in: string)) else {
-            return
-        }
+    private func findAndHighliteText(with patternsWithColors: PatternsWithColors, string: String, attribute: NSMutableAttributedString) {
+        let regex = try? NSRegularExpression(pattern: patternsWithColors.info.pattern, options: [ .anchorsMatchLines])
+        
+        guard let matches = regex?.matches(in: string, options: [], range: NSRange(string.startIndex..., in: string)) else { return }
+        
         for match in matches {
             let range = match.range(at: 0)
-            setUpColorForString(attribute, with: range, with: color)
+            setUpColorForString(attribute, with: range, with: patternsWithColors.info.color)
         }
     }
     
-    private func setUpColorForString(_ attribute: NSMutableAttributedString, with range: NSRange, with color: Constants.Colors) {
-        attribute.addAttribute(NSAttributedString.Key.backgroundColor, value: color.value, range: range)
+    private func setUpColorForString(_ attribute: NSMutableAttributedString, with range: NSRange, with color: UIColor) {
+        attribute.addAttribute(NSAttributedString.Key.backgroundColor, value: color, range: range)
     }
     
     private func setUpColorForView(_ view: UIView, with color: Constants.Colors) {
         view.backgroundColor = color.value
     }
-    
-//    private func choosingSelectedTextRange(str: String) -> ([NSRange], [NSRange]) {
-//        guard let regex = try? NSRegularExpression(pattern: "(\n\\S)") else { return ([], []) }
-//        let match = regex.firstMatch(in: str, options: [], range: NSRange(str.startIndex..., in: str))
-//
-//        let matches = regex.matches(in: str, options: [], range: NSRange(str.startIndex..., in: str))
-//        var strings: [String] = []
-//        var startRange: NSRange? = nil
-//        var endRange = NSRange()
-//        var startDeletedRange: NSRange? = nil
-//        var endDeletedRange = NSRange()
-//        var ranges:([NSRange], [NSRange]) = ([], [])
-//        for m in matches {
-//            let range = m.range(at: 0)
-//            let stringArray = (Array(str)[range.location...(range.location + range.length - 1)])
-//            strings.append(String(stringArray))
-//
-//            if String(stringArray) == "/n-" {
-//                endRange = m.range(at: 0)
-//                startDeletedRange = m.range(at: 0)
-//                if m == matches.last {
-//                    endDeletedRange = m.range(at: 0)
-//                }
-//
-//            } else if String(stringArray) == "/n+" {
-//                endDeletedRange = m.range(at: 0)
-//                startRange = m.range(at: 0)
-//
-//                if m.range.location == matches.last?.range.location {
-//                    endRange = m.range(at: 0)
-//                }
-//            }
-//
-//            if startRange == nil {
-//                guard let startDeletedRange = startDeletedRange else { return ([], []) }
-//                 ranges.1.append(NSRange(location: startDeletedRange.location, length: (endDeletedRange.location - startDeletedRange.location) + (endDeletedRange.length + startDeletedRange.length)))
-//            } else if startDeletedRange == nil {
-//                guard let startRange = startRange else { return ([], []) }
-//                ranges.0.append(NSRange(location: startRange.location, length: (endRange.location - startRange.location) + (endRange.length + startRange.length)))
-//            }
-//
-//        }
-//        return NSRange(location: startRange.location, length: (endRange.location - startRange.location) + (endRange.length + startRange.length))
-//        return ranges
-//    }
     
 }
